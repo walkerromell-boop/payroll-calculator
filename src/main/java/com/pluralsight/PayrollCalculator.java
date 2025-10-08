fc45cb3353c363ae20749d5e8fad8063e5923de1
Romell Walker <walkerromell@gmail.com>
2025-10-08 13:01:24 -0400

    change input to line

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -3,11 +3,12 @@ package com.pluralsight;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 public class PayrollCalculator {
     public static void main(String[] args) {
         Employee[] employees = new Employee[10];
-        int count = 0;
+        int nextEmployee = 0;
 
         try {
             // create a FileReader object connected to the File
@@ -18,23 +19,25 @@ public class PayrollCalculator {
             // read until there is no more data
 
             bufReader.readLine(); //This skips the first line if it's a header
-            while ((line = bufReader.readLine()) != null && count < employees.length) {
-                String[] parts = line.split("\\|");
+            while ((line = bufReader.readLine()) != null ) {
+                String[] parts = line.split(Pattern.quote("|"));
 
                 int id = Integer.parseInt(parts[0]);
                 String name = parts[1];
                 double hoursWorked = Double.parseDouble(parts[2]);
                 double payrate = Double.parseDouble(parts[3]);
-                employees[count]=new Employee(id,name,hoursWorked,payrate);
-                count++;
+                employees[nextEmployee]=new Employee(id,name,hoursWorked,payrate);
+                nextEmployee++;
+
             }
+
             // close the stream and release the resources
             bufReader.close();
         } catch (IOException e) {
             // display stack trace if there was an error
             e.printStackTrace();
         }
-        for (int i=0;i<count;i++){
+        for (int i=0;i<nextEmployee;i++){
             System.out.println(employees[i]);
         }
         
