cc2b888cd3d1b5b314495d004c55bba981813b0f
Romell Walker <walkerromell@gmail.com>
2025-10-08 16:54:19 -0400

    resolve some writer issues

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,26 +1,49 @@
 package com.pluralsight;
 
 import java.io.*;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class PayrollCalculator {
     public static void main(String[] args) {
         Employee[] employees = new Employee[10];
         int nextEmployee = 0;
+        Scanner scanner = new Scanner(System.in);
 
-        readEmployees(employees, nextEmployee);
+        System.out.println("Which file do you want to read?(ex: employees.csv)");
+        String filename = scanner.nextLine();
+//        Read employees from the file the user entered
+        nextEmployee=readEmployees(employees, filename);
+        System.out.println("Enter the name of the New file to create(ex: payroll_report.csv)");
+        String newFilename= scanner.nextLine();
+
+
+//        readEmployees(employees, nextEmployee);
         try {
             // create a FileWriter
-            FileWriter fileWriter = new FileWriter("text.txt", true);
+            FileWriter fileWriter = new FileWriter(newFilename);
             // create a BufferedWriter
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
             // write to the file
-            bufferedWriter.write("line 1 is here \n");
-            bufferedWriter.write("line 2 is here \n");
-            bufferedWriter.write("line 3 is here \n");
+            bufferedWriter.write("Id| Name| Gross Pay|");
+            bufferedWriter.newLine();
 
-            // close the writer
+//            Loop through employees and write each one
+            for (int i = 0; i < nextEmployee; i++) {
+                Employee emp = employees[i];
+
+//                write formatted text to the file
+                String line = String.format("id: %d, name: %s,grosspay:  "
+                        , emp.getEmployeeId(), emp.getName(), emp.getGrosspay());
+
+                bufferedWriter.write(line);
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.newLine();
+            // always close the writer
             bufferedWriter.close();
+            System.out.println("New file created successfully:"+ newFilename);
+
         } catch (IOException e) {
             System.out.println("ERROR: An unexpected error occurred");
             e.getStackTrace();
@@ -39,14 +62,14 @@ public class PayrollCalculator {
             // read until there is no more data
 
             bufReader.readLine(); //This skips the first line if it's a header
-            while ((line = bufReader.readLine()) != null ) {
+            while ((line = bufReader.readLine()) != null) {
                 String[] parts = line.split(Pattern.quote("|"));
 
                 int id = Integer.parseInt(parts[0]);
                 String name = parts[1];
                 double hoursWorked = Double.parseDouble(parts[2]);
                 double payrate = Double.parseDouble(parts[3]);
-                employees[nextEmployee]=new Employee(id,name,hoursWorked,payrate);
+                employees[nextEmployee] = new Employee(id, name, hoursWorked, payrate);
                 nextEmployee++;
 
             }
@@ -57,7 +80,7 @@ public class PayrollCalculator {
             // display stack trace if there was an error
             e.printStackTrace();
         }
-        for (int i = 0; i< nextEmployee; i++){
+        for (int i = 0; i < nextEmployee; i++) {
             System.out.println(employees[i]);
         }
     }
